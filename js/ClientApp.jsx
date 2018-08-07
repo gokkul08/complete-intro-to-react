7eaadf5bb3c6913f6a720f65c893aa9771497184
gokkul08 <agn253@nyu.edu>
2018-08-06 23:47:31 -0400

    Updates from system 1

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
-const ce = React.createElement;
-
-const MyTitle = function(props) {
-  return (
-    <div>
-      <h1 style={{color: props.color}}>{props.title}</h1>
+const App = () => (
+  <HashRouter>
+    <div className='app'>
+      <Route exact path="/" component={Landing} />
+      <Route path='/search' component={Search} />
     </div>
-  );
-};
+    </HashRouter>
+);
 
-const MyFirstComponent = function() {
-  return ce(
-    <div id="my-first-component">
-      <MyTitle title="Game of Thrones" color="YellowGreen" />
-      <MyTitle title="Stranger Things" color="GreenYellow" />
-      <MyTitle title="House of Cards" color="Peru" />
-      <MyTitle title="Daredevil" color="burlywood" />
-    </div>
-  );
-};
-
-render(ce(MyFirstComponent), document.getElementById('app'));
+render(<App />, document.getElementById('app'));
---
+++ js/Search.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Search  = () => <h1>Hi. This is search</h1>;
+
+export default Search;
