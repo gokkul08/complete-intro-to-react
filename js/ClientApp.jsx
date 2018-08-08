81f21bd88f8db0bd3a4cc6dddda47593c261ccac
gokkul08 <agn253@nyu.edu>
2018-08-07 22:53:47 -0400

    Updates from system 1

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -4,14 +4,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 
-const FourOFour = () => <h1>404</h1>
+const FourOFour = () => <h1>404</h1>;
 
 const App = () => (
   <BrowserRouter>
-    <div className='app'>
+    <div className="app">
       <Switch>
         <Route exact path="/" component={Landing} />
-        <Route path='/search' component={Search} />
+        <Route path="/search" component={Search} />
         <Route component={FourOFour} />
       </Switch>
     </div>
