2482f7ce3e3cab1012047af4aba101a4dc29376a
gokkul08 <agn253@nyu.edu>
2018-08-09 00:10:21 -0400

    Updates from system 1

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import ShowCard from './ShowCard';
-import preload from '../data.json';
+import React, { Component } from "react";
+import ShowCard from "./ShowCard";
+import preload from "../data.json";
 
 class Search extends Component {
   state = {
-    searchTerm: ''
+    searchTerm: ""
   };
   handleSearchTermChange = event => {
     this.setState({ searchTerm: event.target.value });
@@ -13,7 +13,7 @@ class Search extends Component {
     return (
       <div className="search">
         <header>
-          <h1>svideo</h1>
+          <h1>{this.state.searchTerm}</h1>
           <input
             onChange={this.handleSearchTermChange}
             value={this.state.searchTerm}
@@ -22,7 +22,14 @@ class Search extends Component {
           />
         </header>
         <div>
-          {preload.shows.map(show => <ShowCard key={show.imdbID} {...show} />)}
+          {preload.shows
+            .filter(
+              show =>
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
+            )
+            .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
       </div>
     );
