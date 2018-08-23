f3673f1c8fbe5ea38307ddb5a627ffa56767c736
gokkul08 <agn253@nyu.edu>
2018-08-22 23:09:34 -0400

    Updates from system 1

--- js/Search.jsx
+++ js/Search.jsx
@@ -27,4 +27,6 @@ const Search = (props: {
 const mapStateToProps = state => ({
   searchTerm: state.searchTerm
 });
+
+export const Unwrapped = Search;
 export default connect(mapStateToProps)(Search);
