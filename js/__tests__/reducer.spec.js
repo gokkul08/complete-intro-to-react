a0429ab1ffc3ae966a390e806e8eb72611d9ef1d
gokkul08 <agn253@nyu.edu>
2018-08-23 08:07:20 -0400

    Updates from system 1

---
+++ js/__tests__/reducer.spec.js
@@ -0,0 +1,48 @@
+// @flow
+
+import reducers from "../reducers";
+
+test("SET_SEARCH_TERM", () => {
+  const state = reducers(
+    { searchTerm: "", apiData: {} },
+    { type: "SET_SEARCH_TERM", payload: "black" }
+  );
+  expect(state).toEqual({ searchTerm: "black", apiData: {} });
+});
+
+test("ADD_API_DATA", () => {
+  const state = reducers(
+    { searchTerm: "west", apiData: {} },
+    {
+      type: "ADD_API_DATA",
+      payload: {
+        rating: "3.4",
+        title: "Westworld",
+        year: "2016–",
+        description: "Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.",
+        poster: "ww.jpg",
+        imdbID: "tt0475784",
+        trailer: "eX3u0IlBBO4"
+      }
+    }
+  );
+  expect(state).toEqual({
+    searchTerm: "west",
+    apiData: {
+      tt0475784: {
+        rating: "3.4",
+        title: "Westworld",
+        year: "2016–",
+        description: "Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.",
+        poster: "ww.jpg",
+        imdbID: "tt0475784",
+        trailer: "eX3u0IlBBO4"
+      }
+    }
+  });
+});
+
+test('ADD_API_DATA with two shows', () => {
+  const state = reducers({searchTerm:'west',apiData:{tt0475784:{rating:'3.4',title:'Westworld',year:'2016–',description:'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',poster:'ww.jpg',imdbID:'tt0475784',trailer:'eX3u0IlBBO4'}}}, {type:'ADD_API_DATA',payload:{rating:'0.0',title:'Game of Thrones',year:'2011–',description:'Nine noble families fight for control over the mythical lands of Westeros, while a forgotten race returns after being dormant for thousands of years.',poster:'got.jpg',imdbID:'tt0944947',trailer:'giYeaKsXnsI'}});
+  expect(state).toEqual({searchTerm:'west',apiData:{tt0475784:{rating:'3.4',title:'Westworld',year:'2016–',description:'Set at the intersection of the near future and the reimagined past, explore a world in which every human appetite, no matter how noble or depraved, can be indulged without consequence.',poster:'ww.jpg',imdbID:'tt0475784',trailer:'eX3u0IlBBO4'},tt0944947:{rating:'0.0',title:'Game of Thrones',year:'2011–',description:'Nine noble families fight for control over the mythical lands of Westeros, while a forgotten race returns after being dormant for thousands of years.',poster:'got.jpg',imdbID:'tt0944947',trailer:'giYeaKsXnsI'}}});
+});
