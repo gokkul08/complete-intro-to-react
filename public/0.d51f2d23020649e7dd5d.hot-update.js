8899d85ac0504e52f9cd7403cee7fdd9c69686c4
gokkul08 <agn253@nyu.edu>
2018-08-26 19:57:49 -0400

    Updates from system 1

---
+++ public/0.d51f2d23020649e7dd5d.hot-update.js
@@ -0,0 +1,10 @@
+webpackHotUpdate(0,{
+
+/***/ "./js/ClientApp.jsx":
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed: SyntaxError: Unexpected token, expected } (21:4)\\n\\n\\u001b[0m \\u001b[90m 19 | \\u001b[39m  module\\u001b[33m.\\u001b[39mhot\\u001b[33m.\\u001b[39maccept(\\u001b[32m'./App'\\u001b[39m\\u001b[33m,\\u001b[39m () \\u001b[33m=>\\u001b[39m {\\n \\u001b[90m 20 | \\u001b[39m    renderApp()\\u001b[33m;\\u001b[39m\\n\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 21 | \\u001b[39m  })\\u001b[33m;\\u001b[39m\\n \\u001b[90m    | \\u001b[39m    \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\n \\u001b[90m 22 | \\u001b[39m}\\n \\u001b[90m 23 | \\u001b[39m\\u001b[0m\\n\");//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9qcy9DbGllbnRBcHAuanN4LmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9");
+
+/***/ })
+
+})
